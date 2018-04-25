ad99933b054f084647cf6ab10dacb9507ebd8300
Ahmed ElSayed <ahmels@microsoft.com>
2018-04-24 18:08:49 -0700

    Update python image setup to work with cli created venv

--- host/2.0/stretch/amd64/python3.6/start.sh
+++ host/2.0/stretch/amd64/python3.6/start.sh
@@ -4,18 +4,26 @@ set -x
 # Directory name for start.sh
 DIR="$(dirname $0)"
 
-# If we're not in a virtual environment, we need to start our host
+# If we're not in a virtual environment, check if either:
+#   $AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT is set, use it for venv
+#   if it's not set and there is a default venv, activate it
+#   else use the container python
 if [ -z "$VIRTUAL_ENV"]
 then
 
-# Determining the virtual environment entry point
-if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT"]
-then
-    AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT='/worker_env/bin/activate'
-fi
+    # Determining the virtual environment entry point
+    if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT" ] && [ -f "$HOME/site/wwwroot/worker_venv/bin/activate" ]
+    then
+        AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT="$HOME/site/wwwroot/worker_venv/bin/activate"
+    fi
 
-echo "activating virtual environment"
-source $AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT
+    if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT" ]
+    then
+        echo "activating virtual environment"
+        source $AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT
+    else
+        echo "using system python"
+    fi
 fi
 
 echo "starting the python worker"
