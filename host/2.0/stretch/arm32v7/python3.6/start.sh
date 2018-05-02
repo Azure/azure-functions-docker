37d8eef6ac7117658632f0efea9770155ad8bf58
Ahmed ElSayed <ahmels@microsoft.com>
2018-05-01 19:23:28 -0700

    Update arm image

--- host/2.0/stretch/arm32v7/python3.6/start.sh
+++ host/2.0/stretch/arm32v7/python3.6/start.sh
@@ -1,25 +1,28 @@
 #! /bin/bash
-set -x
 
 # Directory name for start.sh
 DIR="$(dirname $0)"
 
-# If we're not in a virtual environment, we need to start our host
+# If we're not in a virtual environment, check if either:
+#   $AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT is set, use it for venv
+#   if it's not set and there is a default venv, activate it
+#   else use the container python
 if [ -z "$VIRTUAL_ENV"]
 then
 
-# Determining the virtual environment entry point
-if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT"]
-then
-    AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT='../../worker_env/bin/activate'
-fi
+    # Determining the virtual environment entry point
+    if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT" ] && [ -f "$HOME/site/wwwroot/worker_venv/bin/activate" ]
+    then
+        AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT="$HOME/site/wwwroot/worker_venv/bin/activate"
+    fi
 
-echo "activating virtual environment"
-AZFVENV=''
-AZFVENV+=$DIR
-AZFVENV+='/'
-AZFVENV+=$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT
-source $AZFVENV
+    if [ -z "$AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT" ]
+    then
+        echo "using system python"
+    else
+        echo "activating virtual environment"
+        source $AZURE_FUNCTIONS_VIRTUAL_ENVIRONMENT
+    fi
 fi
 
 echo "starting the python worker"
