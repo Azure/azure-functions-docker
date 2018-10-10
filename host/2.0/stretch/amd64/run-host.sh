2d68bd359d8054188ab9a68e3927da6b8063af5d
Ahmed ElSayed <ahmels@microsoft.com>
2018-10-10 11:10:37 -0700

    Fix run-host.sh

--- host/2.0/stretch/amd64/run-host.sh
+++ host/2.0/stretch/amd64/run-host.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 COUNTER=0
-while [ $COUNTER -lt 10 ]; do
-do
+while [ $COUNTER -lt 5 ]; do
     dotnet /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost.dll
     echo "process exited with $?"
-    echo "restarting host process"
+    echo "restarting host process: $COUNTER"
     let COUNTER=COUNTER+1
 done
