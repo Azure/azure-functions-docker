e88de00002777f44964e2ff5b584c0387b80fc3d
Divya Gandhi <digandhi@microsoft.com>
2018-10-19 11:45:27 -0700

    Adding nodejs images for nanoserver-1803

---
+++ host/2.0/nanoserver-1803/scripts/install_node.ps1
@@ -0,0 +1,24 @@
+<#
+    Installs node in a Docker image
+#>
+
+param (
+    [Parameter(Mandatory = $true)]
+    [String] $Version
+)
+
+$TempDir  ="$($($env:temp))\\"
+$NodeUrl = "https://nodejs.org/dist/v$($Version)/node-v$($Version)-win-x64.zip"
+$NodeZipPath = "$($TempDir)node.zip"
+
+Invoke-WebRequest -Uri $NodeUrl -OutFile $NodeZipPath
+
+Write-Host "Installing node $($Version) "
+try {
+    Expand-Archive $NodeZipPath $TempDir -Force
+    Copy-Item "$($TempDir)node-v$($Version)-win-x64" "C:\\Program Files\\nodejs" -Force -Recurse
+}
+finally {
+    Remove-Item $NodeZipPath -Force -ErrorAction "SilentlyContinue"
+    Remove-Item "$($TempDir)node-v$($Version)-win-x64" -Force -ErrorAction "SilentlyContinue"
+}
