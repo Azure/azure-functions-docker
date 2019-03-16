1ed94f894dc9938c7c83ac2525e3ad27f8d43efb
Ahmed ElSayed <ahmels@microsoft.com>
2019-03-15 17:40:03 -0700

    Update SSH_PORT and PORT

--- host/2.0/stretch/amd64/appservice/start.sh
+++ host/2.0/stretch/amd64/appservice/start.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -z $PORT ]; then
+  ASPNETCORE_URLS=http://*:80
+else
+  ASPNETCORE_URLS=http://*:$PORT
+fi
+
+if [ -z $SSH_PORT ]; then
+  SSH_PORT=2222
+fi
+
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+
 service ssh start
 
 dotnet /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost.dll
