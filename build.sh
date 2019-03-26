e652f5e9ebf78e51cb6fa8e6b94ca90459d1853c
Ahmed ElSayed <ahmels@microsoft.com>
2019-03-26 16:13:32 -0700

    Skip test for java

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ function build_image {
     fi
 
     echo -e "${CONSOLE_BOLD}${COLOR_GREEN}Testing $current_image ${CONSOLE_RESET}"
-    if ! [ -z "$RUN_TESTS" ] && [ "$language" != "base" ]; then
+    if ! [ -z "$RUN_TESTS" ] && [ "$language" != "base" ] && [ "$language" != "java" ]; then
         dotnet run --project $DIR/test/test.csproj $current_image
     fi
 }
