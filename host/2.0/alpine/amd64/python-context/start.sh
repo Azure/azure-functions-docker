f0f7c00884964df246147f3ab5975bea641cc7ae
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-06-17 11:03:29 -0700

    Flip priority of user and system dependencies (#112)

--- host/2.0/alpine/amd64/python-context/start.sh
+++ host/2.0/alpine/amd64/python-context/start.sh
@@ -6,7 +6,7 @@ DIR="$(dirname $0)"
 CUSTOM_PACKAGES_PY36="$HOME/site/wwwroot/.python_packages/lib/python3.6/site-packages"
 CUSTOM_VENV_PACKAGES_PY36="$HOME/site/wwwroot/worker_venv/lib/python3.6/site-packages"
 
-export PYTHONPATH=$PYTHONPATH:$CUSTOM_PACKAGES_PY36:$CUSTOM_VENV_PACKAGES_PY36
+export PYTHONPATH=$CUSTOM_PACKAGES_PY36:$CUSTOM_VENV_PACKAGES_PY36:$PYTHONPATH
 
 echo "python == $(which python)"
 echo "PYTHONPATH == $PYTHONPATH"
