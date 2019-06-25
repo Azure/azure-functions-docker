0eb9dd62dbc394c6df6caa5b984401a002ec2f4b
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-06-24 18:21:58 -0700

    Create package folders for importing

--- host/2.0/stretch/amd64/python-context/start.sh
+++ host/2.0/stretch/amd64/python-context/start.sh
@@ -6,6 +6,10 @@ DIR="$(dirname $0)"
 CUSTOM_PACKAGES_PY36="$HOME/site/wwwroot/.python_packages/lib/python3.6/site-packages"
 CUSTOM_VENV_PACKAGES_PY36="$HOME/site/wwwroot/worker_venv/lib/python3.6/site-packages"
 
+# Needed to import Python packages 
+mkdir -p "$CUSTOM_PACKAGES_PY36"
+mkdir -p "$CUSTOM_VENV_PACKAGES_PY36"
+
 export PYTHONPATH=$CUSTOM_PACKAGES_PY36:$CUSTOM_VENV_PACKAGES_PY36:$PYTHONPATH
 
 echo "python == $(which python)"
