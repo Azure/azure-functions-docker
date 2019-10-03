86a91ec1c058494934e2059f80177e9c21321f80
Ahmed ElSayed <ahmed@elsayed.io>
2019-10-03 12:20:26 -0700

    Update tag & push to match changes in 5d51025

--- build/python.sh
+++ build/python.sh
@@ -85,6 +85,43 @@ function purge {
   docker rmi "${REGISTRY}python:${HOST_VERSION}-python3.7-appservice"
 }
 
+function tag_push {
+  # tag & push build-env images
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.6-buildenv"
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.7-buildenv"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.6-buildenv" "${REGISTRY}python:2.0-python3.6-buildenv"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.7-buildenv" "${REGISTRY}python:2.0-python3.7-buildenv"
+  docker push "${REGISTRY}python:2.0-python3.6-buildenv"
+  docker push "${REGISTRY}python:2.0-python3.7-buildenv"
+
+  # tag & push default python:2.0 and python:2.0-appservice images
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}"
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-appservice"
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-appservice-quickstart"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}"                       "${REGISTRY}python:2.0"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-appservice"            "${REGISTRY}python:2.0-appservice"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-appservice-quickstart" "${REGISTRY}python:2.0-appservice-quickstart"
+  docker push "${REGISTRY}python:2.0"
+  docker push "${REGISTRY}python:2.0-appservice"
+  docker push "${REGISTRY}python:2.0-appservice-quickstart"
+
+  # tag & push default python:2.0-python3.6 and python:2.0-python3.6-appservice images
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.6"
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.6-appservice"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.6"            "${REGISTRY}python:2.0-python3.6"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.6-appservice" "${REGISTRY}python:2.0-python3.6-appservice"
+  docker push "${REGISTRY}python:2.0-python3.6"
+  docker push "${REGISTRY}python:2.0-python3.6-appservice"
+
+  # tag & push default python:2.0-python3.7 and python:2.0-python3.7-appservice images
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.7"
+  docker pull "${REGISTRY}python:${RELEASE_VERSION}-python3.7-appservice"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.7"            "${REGISTRY}python:2.0-python3.7"
+  docker tag  "${REGISTRY}python:${RELEASE_VERSION}-python3.7-appservice" "${REGISTRY}python:2.0-python3.7-appservice"
+  docker push "${REGISTRY}python:2.0-python3.7"
+  docker push "${REGISTRY}python:2.0-python3.7-appservice"
+}
+
 if [ "$1" == "build" ]; then
   build
 elif [ "$1" == "push" ]; then
@@ -95,6 +132,12 @@ elif [ "$1" == "all" ]; then
   build
   push
   purge
+elif [ "$1" == "tag_push" ]; then
+  if [ -z "$RELEASE_VERSION" ]; then
+    echo "ERROR: RELEASE_VERSION is required when running tag_push"
+    exit 1
+  fi
+  tag_push
 else
   echo "Unknown option $1"
   echo "Examples:"
@@ -109,5 +152,8 @@ else
   echo ""
   echo -e "\t$0 all"
   echo -e "\tBuild, push and purge"
+  echo ""
+  echo -e "\t$0 tag_push"
+  echo -e "\tTags \$RELEASE_VERSION images with 2.0 and pushes them"
   echo ""
 fi
