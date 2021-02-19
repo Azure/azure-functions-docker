5e6688f0fcddd58d014abdd2f39c03b6d5db77d8
divyagandhisethi <44176589+divyagandhisethi@users.noreply.github.com>
2021-02-19 11:18:34 -1000

    Update build scripts for autoupgraded v2 apps (#388)

--- host/update-version.ps1
+++ host/update-version.ps1
@@ -13,11 +13,27 @@ if ($MajorVersion -ne "2.0" -And $MajorVersion -ne "3.0") {
     Write-Error "MajorVersion can only be 2.0 or 3.0. Got $MajorVersion" -ErrorAction Stop
 }
 
-$dockerFiles = Get-ChildItem  ./$MajorVersion/*DockerFile -rec
-foreach ($f in $dockerFiles)
-{
-    $content = Get-Content $f.PSPath
-    Write-Host "Updating version in $($f.PSPath) to $FullVersion"
-    $newContent = ($content -replace "HOST_VERSION=$MajorVersion\.(\d+)", "HOST_VERSION=$FullVersion" -join "`n") + "`n"
-    Set-Content -Path $f.PSPath -Value $newContent -NoNewline
+function Update-HostVersion {
+     param (
+        $MajorVersion,
+        $Directory
+    )
+    
+    $dockerFiles = Get-ChildItem  ./$Directory/*DockerFile -rec
+    foreach ($f in $dockerFiles)
+    {
+        $content = Get-Content $f.PSPath
+        Write-Host "Updating version in $($f.PSPath) to $FullVersion"
+        $newContent = ($content -replace "HOST_VERSION=$MajorVersion\.(\d+)", "HOST_VERSION=$FullVersion" -join "`n") + "`n"
+        Set-Content -Path $f.PSPath -Value $newContent -NoNewline
+    }
 }
+
+Update-HostVersion -MajorVersion $MajorVersion -Directory $MajorVersion
+
+if ($MajorVersion -eq "3.0") {
+    Write-Host "Updating auto-upgraded files"
+    Update-HostVersion -MajorVersion $MajorVersion -Directory "2.0-upgrade"
+}
+    
+    
