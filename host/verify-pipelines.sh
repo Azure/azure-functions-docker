9f2b04a45b0384d93c5396a2417e05c820724869
Cooper Link <cooperlink@microsoft.com>
2021-09-17 11:02:20 -0700

    Testing on GH Actions

---
+++ host/verify-pipelines.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+DIR=$(dirname $0)
+IFS=$'\n'
+
+if [ "$1" != "2.0" ] && [ "$1" != "3" ] && [ "$1" != "4" ]; then
+  echo -e "MajorVersion must be 2.0, 3., or 4. Got '$1'\n"
+  print_usage
+  exit 1
+fi
+
+if [ "$1" == "2.0" ]; then
+   echo -e "MajorVersion 2.0 is not supported by this script. Continuing with NoOp"
+   exit 0
+fi
+
+if [ "$1" == "3" ]; then
+   echo -e "MajorVersion 3 detected. Verifying pipelines in host/3.0/"
+   TARGET="./3.0"
+else 
+   echo -e "MajorVersion 4 detected. Verifying pipelines in host/4/"
+   TARGET="./4"
+fi
+
+stage=($(((grep "docker push".*appservice ${TARGET}/publish-appservice-stage.yml) | awk -F'-appservice' '{print $1}') | awk -F 'TARGET_REGISTRY' '{print $2}') )
+
+pub=($((((grep "docker push".*appservice  ${TARGET}/publish.yml)| grep -v quickstart) | awk -F'-appservice' '{print $1}') | awk -F 'TARGET_REGISTRY' '{print $2}') )
+
+difference=($(comm -3 <(printf '%s\n' "${stage[@]}"|sort -u) <(printf '%s\n' "${pub[@]}"|sort -u)))
+
+if [ -z "$difference" ]; then
+  echo -e "No differences between Stage and Publish pipelines. Rollout Safe'\n"
+  exit 0
+fi
+
+if [ ${#difference[@]} == 1 ] && [[ $difference =~ "base" ]]; then
+   echo -e "Stage pipeline excludes base image. This is a known difference. Rollout Safe"
+   exit 0
+else 
+   echo -e "Publish and stage pipelines inconsistent. Other than the /base image the pipelines should publish the same -appservice images. The following differences exist for -appservice images between the pipelines: "
+   echo -e ${difference[@]}
+   exit 1
+fi
+
