d9dbbfe9aa992569d6125a5516741665b40da315
Cooper Link <cooperlink@microsoft.com>
2021-10-01 08:03:50 -0700

    Completed Java testing

--- host/3.0/buster/amd64/base/generate-composite.sh
+++ host/3.0/buster/amd64/base/generate-composite.sh
@@ -9,22 +9,46 @@ globalize_args() {
     ## This method is used to bring all args preceding any From statements to the top of the file. 
     ## Allows for ARGs defined globally in composite images to be global in the compiled dockerfile
     gblarg=($((sed -n '/FROM/q;p' $1) | grep ARG)) # list of args in arr with carriage returns at end
+    printf "\t ARGs globalized for $1 : \n"
+    for arg in ${gblarg[@]};
+    do
+        printf "\t\t $arg \n"
+    done 
+
 }
 
 combine_dockerfile() {
-    echo $"Combining $base with $1 \n"
-    filename=$1
-    echo $1 | cut -d'/' -f 3
-    
+    printf $"  Combining $base with $1\n"
+    runtime=`echo $1 | cut -d'/' -f 2`
+    runtimeversion=`echo $1 | cut -d'/' -f 3`
+    printf "\t Image Runtime and Version: $runtimeversion \n"
 
-    echo $"Collecting global arguments in $1 to globalize them in the final dockerfile.\n"
+    printf $"\t Collecting global arguments in $1 to globalize them in the final dockerfile.\n"
     globalize_args $1
     printf '%s\n' "${gblarg[@]}" > ./temp.txt
-    cat ./temp.txt $base $1 > ./out/java/java11-appservice.Dockerfile
+    cat ./temp.txt $base $1 > ./out/${runtime}/${runtimeversion}-appservice.Dockerfile
+    printf $"\t Completed compilation of $runtimeversion. Output file available at : ./out/${runtime}/${runtimeversion}-appservice.Dockerfile\n"
 }
 
-base="./host.Dockerfile"
+generate_appservice() {
+    base="./host.Dockerfile"
+    echo $"Generating Appservice Images for [ $@ ] using base $base"
+    for lang in $@; 
+    do 
+        langimgs=($(find ../$lang/ -name "*-composite.Dockerfile"))
+        if [ -z "$langimgs" ]; then
+            echo "No composite images found for language : $lang. Skipping compilation..."
+        else
+            echo $"Found following images for compilation of language $lang: ${langimgs[@]}"
+            for version in ${langimgs[@]};
+            do
+                combine_dockerfile $version
+                printf "\n"
+            done
+            printf $"Completed compilation of $lang images \n\n"
+        fi
+    done
+    echo "Compilation of all images in list: [ $@ ] complete."
+}
 
-langimgs=($(find ../java/ -name "*-composite.Dockerfile"))
-
-combine_dockerfile ${langimgs[@]}
+generate_appservice "java" "python"
--- host/3.0/buster/amd64/java/java8/java8-composite.Dockerfile
+++ host/3.0/buster/amd64/java/java8/java8-composite.Dockerfile
@@ -1,5 +1,4 @@
 ARG JAVA_VERSION=8u252
-
 FROM mcr.microsoft.com/java/jre-headless:${JAVA_VERSION}-zulu-debian10-with-tools as jre
 FROM mcr.microsoft.com/dotnet/core/runtime-deps:3.1
 ARG HOST_VERSION
@@ -16,8 +15,10 @@ COPY --from=runtime-image [ "/azure-functions-host", "/azure-functions-host" ]
 COPY --from=runtime-image [ "/workers/java", "/azure-functions-host/workers/java" ]
 COPY --from=jre [ "/usr/lib/jvm/zre-hl-8-azure-amd64", "/usr/lib/jvm/zre-8-azure-amd64" ]
 
-COPY sshd_config /etc/ssh/
-COPY start.sh /azure-functions-host/
+## Using this method with ../ requires the docker build be run from the /out/ folder.
+## Alternative solution would be to copy the main files for sshd and start.sh to each lang folder
+COPY ../shared/sshd_config /etc/ssh/
+COPY ../shared/start.sh /azure-functions-host/
 COPY --from=runtime-image [ "/FuncExtensionBundles", "/FuncExtensionBundles" ]
 
 ENV JAVA_HOME /usr/lib/jvm/zre-8-azure-amd64
