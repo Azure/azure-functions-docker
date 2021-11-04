f41db6018d6ac037fbba2b5c3a05971774253e8c
CooperLink <46761504+CooperLink@users.noreply.github.com>
2021-11-04 15:59:59 -0700

    Templating  Dockerfiles - JAVA (#516)
    * combine logical units in java
    
    * Completed Java testing
    
    * Template files, test dir, gitignore, remove temp
    
    * Included sharedconfig to simplify build context
    
    * Updated Java Build Pipeline
    
    * Updated host script to run anywhere
    
    * Updated execution permissions
    
    * Remove node, update dotnetsdk, testing java images
    
    * Updated flag with release mode. Removed test
    
    * Update release to generate images
    
    * Generate release artifacts
    
    * Manually remove /out/ folders
    
    * Remove now un-used images
    
    * Updated ext and host ver
    
    Co-authored-by: Cooper Link <cooperlink@microsoft.com>

---
+++ host/3.0/buster/amd64/base/generate-composite.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+
+
+
+DIR=$(dirname $0)
+IFS=$'\n'
+
+globalize_args() {
+    ## This method is used to bring all args preceding any From statements to the top of the file. 
+    ## Allows for ARGs defined globally in composite images to be global in the compiled dockerfile
+    gblarg=($((sed -n '/FROM/q;p' $1) | grep ARG)) # list of args in arr with carriage returns at end
+    if [ ${#gblarg[@]} == 0 ]; then 
+        printf "\t No global ARGs in $1 \n"
+    else
+        printf "\t ARGs globalized for $1 : \n"
+        for arg in ${gblarg[@]};
+        do
+            printf "\t\t $arg \n"
+        done 
+    fi
+}
+
+combine_dockerfile() {
+    printf $"  Combining $base with $1\n"
+    localfile=$(printf $1 | awk -F'\\.\\.' '{print $2}')
+    runtime=`echo $localfile | cut -d'/' -f 2`
+    runtimeversion=`echo $localfile | cut -d'/' -f 3`
+    printf "\t Image Runtime and Version: $runtimeversion \n"
+
+    # Output path
+    
+
+    # File structure creation
+    outputdir=$"$DIR/../out/${runtime}"
+    if [ "$RELEASE" == "true" ]; then
+        printf "\t Release Mode enabled. Updating release dockerfiles.\n"
+        outputdir=$"$DIR/../release/${runtime}"
+    fi
+    mkdir -p $outputdir
+    outputfile="$outputdir/${runtimeversion}-appservice.Dockerfile"
+
+    printf $"\t Collecting global arguments in $1 to globalize them in the final dockerfile.\n"
+    globalize_args $1
+    echo "${gblarg[@]}" | cat - $base $1 > $outputfile
+
+    printf $"\t Completed compilation of $runtimeversion. Output file available at : $outputfile\n"
+}
+
+generate_appservice() {
+    base="$DIR/host.Dockerfile"
+    echo $"Generating Appservice Images for [ $@ ] using base $base"
+    for lang in $@; 
+    do 
+        langimgs=($(find $DIR/../$lang/ -name "*-composite.template"))
+        if [ -z "$langimgs" ]; then
+            echo "No composite images found for language : $lang. Skipping compilation..."
+        else
+            echo $"Found following images for compilation of language $lang: ${langimgs[@]}"
+            for version in ${langimgs[@]};
+            do
+                combine_dockerfile $version
+                printf "\n"
+            done
+            printf $"Completed compilation of $lang images \n\n"
+        fi
+    done
+    echo "Compilation of all images in list: [ $@ ] complete."
+    printf "\n"
+}
+
+copy_shared_config() {
+     
+    
+    echo "Copying shared config files into the following language output directories: "
+    for lang in $@;
+    do
+        outputdir=$"$DIR/../out/${lang}/"
+        if [ "$RELEASE" == "true" ]; then
+            outputdir=$"$DIR/../release/${lang}/"
+        fi
+        printf "\t $outputdir\n"
+        cp -R $DIR/sharedconfig/* $outputdir
+    done
+}
+
+clear_outputdir() {
+    echo "Clearing output folder..."
+    rootdir=$"$DIR/../out/*"
+    rm -rf rootdir
+}
+
+echo "Dir is : $DIR"
+
+# Defines a flag -r : Release Mode.  Release mode will generate artifacts in a folder titled /release/
+# These .Dockerfile artifacts will be used to create the containers uploaded to ACR. Increases trace-ability.
+while getopts "r" flag;do
+    case ${flag} in
+      r)
+        RELEASE="true"
+        echo "Release Mode is enabled"
+        ;;
+    esac
+done
+
+if [ -z $RELEASE ]; then
+    clear_outputdir
+fi
+
+# Remove flags from input. Store args in array
+declare -a argarray
+for arg do
+    if [ "${arg:0:1}" != '-' ]; then
+        argarray+=($arg)
+    fi
+done
+
+if [ ${#argarray[@]} == 0 ]; then
+    echo "No language arguments supplied for compilation. Please supply a supported language or 'all' as an input."
+    exit 1
+fi
+
+if [ ${#argarray[@]} == 1 ] && [ "$argarray" == "all" ]; then
+    echo "All supported languages targetted."
+    supportedlangs=("java")
+    argarray=(${supportedlangs[@]})
+fi
+
+generate_appservice ${argarray[@]}
+
+copy_shared_config ${argarray[@]}
--- host/3.0/buster/amd64/base/host.Dockerfile
+++ host/3.0/buster/amd64/base/host.Dockerfile
@@ -1,13 +1,14 @@
-# Build the runtime from source
+
 ARG HOST_VERSION=3.3.1
-ARG JAVA_VERSION=8u252
+
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS runtime-image
 ARG HOST_VERSION
 
 ENV PublishWithAspNetCoreTargetManifest=false
-RUN echo "HOST_VARSION: "${HOST_VERSION}
+ENV DEBIAN_FRONTEND=noninteractive
+
 RUN BUILD_NUMBER=$(echo ${HOST_VERSION} | cut -d'.' -f 3) && \
-    git clone --branch v${HOST_VERSION} https://github.com/Azure/azure-functions-host /src/azure-functions-host && \
+    git clone --branch v${HOST_VERSION} https://github.com/Azure/azure-functions-host.git /src/azure-functions-host && \
     cd /src/azure-functions-host && \
     HOST_COMMIT=$(git rev-list -1 HEAD) && \
     dotnet publish -v q /p:BuildNumber=$BUILD_NUMBER /p:CommitHash=$HOST_COMMIT src/WebJobs.Script.WebHost/WebJobs.Script.WebHost.csproj -c Release --output /azure-functions-host --runtime linux-x64 && \
@@ -36,32 +37,3 @@ RUN EXTENSION_BUNDLE_VERSION=1.8.1 && \
     rm -f /$EXTENSION_BUNDLE_FILENAME_V3 &&\
     find /FuncExtensionBundles/ -type f -exec chmod 644 {} \;
 
-FROM mcr.microsoft.com/java/jre-headless:${JAVA_VERSION}-zulu-debian10-with-tools as jre
-FROM mcr.microsoft.com/dotnet/core/runtime-deps:3.1
-ARG HOST_VERSION
-
-EXPOSE 2222 80
-
-ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
-    HOME=/home \
-    FUNCTIONS_WORKER_RUNTIME=java \
-    DOTNET_USE_POLLING_FILE_WATCHER=true \
-    HOST_VERSION=${HOST_VERSION}
-
-COPY --from=runtime-image [ "/azure-functions-host", "/azure-functions-host" ]
-COPY --from=runtime-image [ "/workers/java", "/azure-functions-host/workers/java" ]
-COPY --from=jre [ "/usr/lib/jvm/zre-hl-8-azure-amd64", "/usr/lib/jvm/zre-8-azure-amd64" ]
-
-COPY sshd_config /etc/ssh/
-COPY start.sh /azure-functions-host/
-COPY --from=runtime-image [ "/FuncExtensionBundles", "/FuncExtensionBundles" ]
-
-ENV JAVA_HOME /usr/lib/jvm/zre-8-azure-amd64
-
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends openssh-server dialog && \
-    echo "root:Docker!" | chpasswd
-
-RUN chmod +x /azure-functions-host/start.sh
-
-ENTRYPOINT ["/azure-functions-host/start.sh"]
