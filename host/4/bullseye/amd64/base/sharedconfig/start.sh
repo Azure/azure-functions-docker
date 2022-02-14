518d65417ca9e1820047057b57b0098ca2c0a532
Filipe Farinha <ffarinha@microsoft.com>
2022-02-14 17:38:38 +0000

    export of env variables to show up in SSH

--- host/4/bullseye/amd64/base/sharedconfig/start.sh
+++ host/4/bullseye/amd64/base/sharedconfig/start.sh
@@ -17,6 +17,10 @@ if [ "$APPSVC_REMOTE_DEBUGGING" == "TRUE" ]; then
     export languageWorkers__python__arguments="-m ptvsd --host localhost --port $APPSVC_TUNNEL_PORT"
 fi
 
+# Get environment variables to show up in SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+# starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 
 service ssh start
