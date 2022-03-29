28346f7a7005d5b87260293df77cc3580aedc773
Cooper Link <cooperlink@microsoft.com>
2022-03-29 14:36:14 -0700

    Fix powershell tests

--- test/index.ts
+++ test/index.ts
@@ -195,10 +195,7 @@ async function main() {
     await runTest(testData.node, "-e FUNCTIONS_WORKER_RUNTIME=node");
     await runTest(testData.python, "-e FUNCTIONS_WORKER_RUNTIME=python");
     await runTest(testData.java, "-e FUNCTIONS_WORKER_RUNTIME=java");
-
-    // Enable PowerShell tests for V4 only - https://github.com/Azure/azure-functions-docker/issues/640
-    // await runTest(testData.powershell, "-e FUNCTIONS_WORKER_RUNTIME=powershell -e FUNCTIONS_WORKER_RUNTIME_VERSION=7");
-    // await runTest(testData.powershell, "-e FUNCTIONS_WORKER_RUNTIME=powershell -e FUNCTIONS_WORKER_RUNTIME_VERSION=7.2");
+   await runTest(testData.powershell, "-e FUNCTIONS_WORKER_RUNTIME=powershell");
   }
 }
 
