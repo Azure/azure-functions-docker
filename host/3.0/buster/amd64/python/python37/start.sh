385096c6d7733aa9cc7b53d4ccf430bc75450979
Zachary Halzel <zahalzel@microsoft.com>
2023-11-17 10:58:19 -0500

    Remove new line

--- host/3.0/buster/amd64/python/python37/start.sh
+++ host/3.0/buster/amd64/python/python37/start.sh
@@ -23,8 +23,7 @@ service ssh start
 
 echo -e "**WARNING**: You are using an outdated version of the Azure Functions runtime:
 https://learn.microsoft.com/en-us/azure/azure-functions/migrate-version-3-version-4
-====================================================================================
-"
+===================================================================================="
 
 
 if [ -f /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost ]; then
