0c7da791e0ca059b868f74ff70aff39d945dc4cc
Zachary Halzel <zahalzel@microsoft.com>
2023-11-20 15:14:38 -0500

    Refactor scripts

--- host/3.0/buster/arm32v7/dotnet/start_deprecated.sh
+++ host/3.0/buster/arm32v7/dotnet/start_deprecated.sh
@@ -2,4 +2,9 @@
 echo -e "**WARNING**: You are using an outdated version of the Azure Functions runtime:
 https://learn.microsoft.com/en-us/azure/azure-functions/migrate-version-3-version-4
 ===================================================================================="
-dotnet /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost.dll
+
+if [ -f /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost ]; then
+    /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost
+else
+    dotnet /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost.dll
+fi
