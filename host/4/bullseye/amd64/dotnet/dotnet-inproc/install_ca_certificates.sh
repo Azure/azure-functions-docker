b9e82134f9ba01fa0d143e78861374cb2ee1e0de
Cooper Link <cooperlink@microsoft.com>
2024-05-08 12:36:21 -0700

    Update with conditional

--- host/4/bullseye/amd64/dotnet/dotnet-inproc/install_ca_certificates.sh
+++ host/4/bullseye/amd64/dotnet/dotnet-inproc/install_ca_certificates.sh
@@ -3,6 +3,7 @@
 # Source and destination directories
 source_dir="/var/ssl/root"
 destination_dir="/usr/local/share/ca-certificates"
+need_certificate_update=false
 
 if [[ "$WEBSITES_INCLUDE_CLOUD_CERTS" == "true" ]]; then
     echo "WEBSITES_INCLUDE_CLOUD_CERTS is set to true."
@@ -10,8 +11,7 @@ if [[ "$WEBSITES_INCLUDE_CLOUD_CERTS" == "true" ]]; then
     if [ "$(ls "$agc_source_dir"/*.crt 2>/dev/null)" ]; then
       # Copy CA certificates
       cp "$agc_source_dir"/*.crt "$destination_dir"
-      # Run update-ca-certificates command to update the CA certificate store
-      update-ca-certificates
+      need_certificate_update=true
     fi
 else
     echo "WEBSITES_INCLUDE_CLOUD_CERTS is not set to true."
@@ -24,8 +24,12 @@ if [ "$(ls "$source_dir"/*.crt 2>/dev/null)" ]; then
    # Copy CA certificates
    cp "$source_dir"/*.crt "$destination_dir"
    
-   # Run update-ca-certificates command to update the CA certificate store
-   update-ca-certificates
+   need_certificate_update=true
 
    echo "CA certificates copied and updated successfully."
+fi
+
+if $need_certificate_update; then
+   # Run update-ca-certificates command to update the CA certificate store
+   update-ca-certificates
 fi
