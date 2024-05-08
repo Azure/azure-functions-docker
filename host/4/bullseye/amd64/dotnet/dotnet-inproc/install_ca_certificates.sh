f77c2a2acb3b9efadf8efdd96954ed194aafad99
Cooper Link <cooperlink@microsoft.com>
2024-05-08 11:51:44 -0700

    Update dotnet

--- host/4/bullseye/amd64/dotnet/dotnet-inproc/install_ca_certificates.sh
+++ host/4/bullseye/amd64/dotnet/dotnet-inproc/install_ca_certificates.sh
@@ -4,6 +4,20 @@
 source_dir="/var/ssl/root"
 destination_dir="/usr/local/share/ca-certificates"
 
+if [[ "$WEBSITES_INCLUDE_CLOUD_CERTS" == "true" ]]; then
+    echo "WEBSITES_INCLUDE_CLOUD_CERTS is set to true."
+    agc_source_dir="/usr/local/azure/certs"
+    if [ "$(ls "$agc_source_dir"/*.crt 2>/dev/null)" ]; then
+      # Copy CA certificates
+      cp "$agc_source_dir"/*.crt "$destination_dir"
+      # Run update-ca-certificates command to update the CA certificate store
+      update-ca-certificates
+    fi
+else
+    echo "WEBSITES_INCLUDE_CLOUD_CERTS is not set to true."
+fi
+
+
 # Check if the source directory has no files with the .crt extension
 if [ "$(ls "$source_dir"/*.crt 2>/dev/null)" ]; then
    
