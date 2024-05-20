2e03bb78e887b6cca7a16008ad690b4753cd0b79
CooperLink <46761504+CooperLink@users.noreply.github.com>
2024-05-20 10:21:18 -0700

    Add AGC certificates to CA-certificates when WEBSITES_USE_CLOUD_CERTS is set to true. (#1077)
    * Update dotnet
    
    * Update with conditional
    
    * Update install_ca_certs for all
    
    ---------
    
    Co-authored-by: Cooper Link <cooperlink@microsoft.com>

--- host/4/bullseye/amd64/node/node18/install_ca_certificates.sh
+++ host/4/bullseye/amd64/node/node18/install_ca_certificates.sh
@@ -3,6 +3,20 @@
 # Source and destination directories
 source_dir="/var/ssl/root"
 destination_dir="/usr/local/share/ca-certificates"
+need_certificate_update=false
+
+if [[ "$WEBSITES_INCLUDE_CLOUD_CERTS" == "true" ]]; then
+    echo "WEBSITES_INCLUDE_CLOUD_CERTS is set to true."
+    agc_source_dir="/usr/local/azure/certs"
+    if [ "$(ls "$agc_source_dir"/*.crt 2>/dev/null)" ]; then
+      # Copy CA certificates
+      cp "$agc_source_dir"/*.crt "$destination_dir"
+      need_certificate_update=true
+    fi
+else
+    echo "WEBSITES_INCLUDE_CLOUD_CERTS is not set to true."
+fi
+
 
 # Check if the source directory has no files with the .crt extension
 if [ "$(ls "$source_dir"/*.crt 2>/dev/null)" ]; then
@@ -10,8 +24,12 @@ if [ "$(ls "$source_dir"/*.crt 2>/dev/null)" ]; then
    # Copy CA certificates
    cp "$source_dir"/*.crt "$destination_dir"
    
-   # Run update-ca-certificates command to update the CA certificate store
-   update-ca-certificates
+   need_certificate_update=true
 
    echo "CA certificates copied and updated successfully."
+fi
+
+if $need_certificate_update; then
+   # Run update-ca-certificates command to update the CA certificate store
+   update-ca-certificates
 fi
