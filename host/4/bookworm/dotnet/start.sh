61f370db7799c7d2679560411dd0ec407c879789
CooperLink <46761504+CooperLink@users.noreply.github.com>
2024-06-07 10:36:13 -0700

    Revert "Revert ".NET 8 in proc changes for dedicated linux images (#1069)" (#…"
    This reverts commit e7aefd2dd556a1b25db54cf42b18de9768ba2cb3.

---
+++ host/4/bookworm/dotnet/start.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export DOTNET_USE_POLLING_FILE_WATCHER=true
+
+if [ -z $PORT ]; then
+  export ASPNETCORE_URLS=http://*:80
+else
+  export ASPNETCORE_URLS=http://*:$PORT
+fi
+
+# Install ca-certificates
+source /opt/startup/install_ca_certificates.sh
+
+if [ -z $SSH_PORT ]; then
+  export SSH_PORT=2222
+fi
+
+if [ "$APPSVC_REMOTE_DEBUGGING" == "TRUE" ]; then
+    export languageWorkers__node__arguments="--inspect=0.0.0.0:$APPSVC_TUNNEL_PORT"
+    export languageWorkers__python__arguments="-m ptvsd --host localhost --port $APPSVC_TUNNEL_PORT"
+fi
+
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+
+service ssh start
+
+if [ -f /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost ]; then
+    /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost
+else
+    dotnet /azure-functions-host/Microsoft.Azure.WebJobs.Script.WebHost.dll
+fi
